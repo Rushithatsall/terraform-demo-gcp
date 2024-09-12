baa173096dd0b84747b38455e1d33a1d0bd28b80
Rushithatsall <103929964+Rushithatsall@users.noreply.github.com>
2024-09-12 19:35:34 +0530

    destroy

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-## the bucketname is prefixed with the GCP project name which is unique
+/*## the bucketname is prefixed with the GCP project name which is unique
 resource "google_storage_bucket" "auto-expire" {
   name          = "ringed-inn-430209-i8-bucket"
   location      = "US"
